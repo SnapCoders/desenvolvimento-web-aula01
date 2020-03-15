115e8e5180b0ca17040b3364d14c3ef47cc23254
Bruno Futema <bruno.futema@outlook.com>
2020-03-14 21:57:25 -0300

    feature: e) alterando as classes Retangulo e Quadrado para implementar a classe Diagonal e implementar o método que calcula a diagonal

--- src/br/com/desenvolvimentoweb/model/Quadrado.java
+++ src/br/com/desenvolvimentoweb/model/Quadrado.java
@@ -1,6 +1,6 @@
 package br.com.desenvolvimentoweb.model;
 
-public class Quadrado extends Poligono {
+public class Quadrado extends Poligono implements Diagonal {
 	public Quadrado(double base, double altura) {
 		super(base, altura);
 	}
@@ -8,5 +8,10 @@ public class Quadrado extends Poligono {
 	@Override
 	public double area() {
 		return Math.pow(getBase(), 2);
+	}
+	
+	@Override
+	public double calculaDiagonal() {
+		return Math.sqrt(this.area()) * Math.sqrt(2);
 	}
 }
